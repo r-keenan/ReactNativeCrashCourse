c8745caab2612bf848036ed51ea4bac29ddf9250
Ross Keenan <ross.keenan@protonmail.com>
2021-06-13 18:30:38 -0500

    Changed function name and default export to Header

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,18 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Header;
