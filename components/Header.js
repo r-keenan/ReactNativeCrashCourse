999d98bb39200a547e072829bd9f83747d8bc659
Ross Keenan <ross.keenan@protonmail.com>
2021-06-13 18:32:49 -0500

    Set styling for Header

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,22 @@ import { View, Text, StyleSheet } from "react-native";
 
 const Header = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View style={styles.header}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue",
+  },
+  text: {
+    color: "white",
+    fontSize: 23,
+    textAlign: "center",
   },
 });
 
