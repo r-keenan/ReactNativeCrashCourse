2dcf32387f95ff11d93d9f3cd408372c14fcbc86
Ross Keenan <ross.keenan@protonmail.com>
2021-06-13 19:38:51 -0500

    Created AddItem component.

---
+++ components/AddItems.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const AddItem = ({ title }) => {
+  return (
+    <View>
+      <TextInput placeholder="Add Items..." style={styles.input} />
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>
+          <Icon name="plus" size={20} />
+          Add Item
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  btn: {
+    backgroundColor: "#c2bad8",
+    padding: 9,
+    margin: 5,
+  },
+  btnText: {
+    color: "darkblueslate",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
+
+export default AddItem;
