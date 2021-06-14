0aa92c8969aa96005b4305fa9008cd7d62f7f677
Ross Keenan <ross.keenan@protonmail.com>
2021-06-13 19:24:48 -0500

    Now able to delete items.

--- App.js
+++ App.js
@@ -9,22 +9,30 @@ const App = () => {
   const [items, setItems] = useState([
     {
       id: uuidv4(),
+      text: "Juice",
+      id: uuidv4(),
       text: "Milk",
       id: uuidv4(),
       text: "Eggs",
       id: uuidv4(),
       text: "Bread",
-      id: uuidv4(),
-      text: "Juice",
     },
   ]);
 
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Header title="Shopping List" />
+      <Header />
       <FlatList
         data={items}
-        renderItem={({ item }) => <ListItem item={item} />}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
