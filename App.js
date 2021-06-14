c31699a0f2ef19e630fe3d28431b143dcb7e21fe
Ross Keenan <ross.keenan@protonmail.com>
2021-06-13 19:14:34 -0500

    Created styling for listitems and listitem component.

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import Header from "./components/Header";
+import "react-native-get-random-values";
+import ListItem from "./components/ListItem";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuidv4(),
+      text: "Milk",
+      id: uuidv4(),
+      text: "Eggs",
+      id: uuidv4(),
+      text: "Bread",
+      id: uuidv4(),
+      text: "Juice",
+    },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Header title='Shopping List'/>
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} />}
+      />
     </View>
   );
 };
